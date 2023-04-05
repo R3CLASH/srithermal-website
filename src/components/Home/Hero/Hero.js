155b0c1732bb76b4263e90301e107fd122cc9d25
Thavisha Mudunkotuwa <90696020+R3CLASH@users.noreply.github.com>
2023-04-06 02:27:43 +0530

    Update Hero.js

--- src/components/Home/Hero/Hero.js
+++ src/components/Home/Hero/Hero.js
@@ -3,13 +3,13 @@ import Link from "next/link";
 // import "./Hero.css";
 function Hero(props) {
   return (
-    <div className="max-w- flex items-center justify-center h-screen hero-img bg-cover bg-center lg:block lg:p-60 lg:px-20">
-      <div className="hero-content p-10 lg:pt-10 lg:w-1/2 lg:pr-20">
-        <h1 className="text-5xl text-white text-left font-semibold">
+    <div className="flex items-center justify-center h-screen hero-img bg-cover bg-center lg:block lg:p-60 lg:px-20 px-10">
+      <div className="hero-content lg:pt-10 lg:w-1/2 lg:pr-20">
+        <h1 className="text-4xl lg:text-5xl text-white text-left font-semibold">
           {props.heading}
         </h1>
         <p className="text-white text-lg pt-5">{props.paragraph}</p>
-        <Link href="/contact">
+        <Link href="/contact" className="">
           <button className="border mt-5 px-8 py-2 text-white text-xl text-center">
             {props.btnText}
           </button>
