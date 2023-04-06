2ef422cc76f445a19ede74a8f0f333699d457f35
Thavisha <thavisha@surge.global>
2023-04-07 02:10:24 +0530

    After footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="w-full text-center bg-slate-900 text-white lg:py-10 lg:text-xl text-base py-5">
+      Copyright © 2023 SRI THERMAL ENGINEERING | Developed by PolkaDevz Theme
+    </div>
+  );
+}
+
+export default Footer;
